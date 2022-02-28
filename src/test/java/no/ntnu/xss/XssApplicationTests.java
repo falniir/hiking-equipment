e77dd4c13b8081b750ad163c3c59f7b87aa576a4
Eduard Andrei Cristea <falniir@gmail.com>
2022-03-01 00:54:14 +0100

    Initial commit

---
+++ src/test/java/no/ntnu/xss/XssApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.xss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
